e0d5f093fd008711209d4929228d7a77b92de174
Stefani Celestino Rees <stefani.rees@linx.com.br>
2026-04-29 19:22:31 -0300

    test(payment): add unit tests for calcularTaxa

--- tests/payment.test.js
+++ tests/payment.test.js
@@ -1,5 +1,5 @@
 // tests/payment.test.js
-const { validarCartao, calcularParcelas } = require("../src/payment");
+const { validarCartao, calcularParcelas, calcularTaxa } = require("../src/payment");
 
 describe("validarCartao", () => {
   test("aceita cartão com 16 dígitos", () => {
@@ -18,5 +18,17 @@ describe("calcularParcelas", () => {
 
   test("6x com juros de 2%", () => {
     expect(calcularParcelas(300, 6)).toBeCloseTo(51, 0);
+  });
+});
+
+describe("calcularTaxa", () => {
+  test("aplica 3% para crédito", () => {
+    expect(calcularTaxa("credito", 100)).toBeCloseTo(3);
+  });
+  test("aplica R$3,50 fixo para boleto", () => {
+    expect(calcularTaxa("boleto", 100)).toBe(3.5);
+  });
+  test("retorna 0 para outros métodos", () => {
+    expect(calcularTaxa("pix", 100)).toBe(0);
   });
 });
